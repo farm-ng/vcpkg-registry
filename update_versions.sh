ff1b1c32c3997ab5b7da2e8a55f2a528e73668f5
Ian Sherman <ian.j.sherman@gmail.com>
2024-01-02 12:13:49 -0800

    Update versions

--- update_versions.sh
+++ update_versions.sh
@@ -3,7 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Expects vcpkg to be installed as a submodule
-./vcpkg/bootstrap-vcpkg.sh
+# git submodule update --init --recursive
+# ./vcpkg/bootstrap-vcpkg.sh
+
+# Format the manifests
+find $DIR/ports -name "vcpkg.json" | xargs vcpkg/vcpkg format-manifest
 
 # Update the versions database
 # https://learn.microsoft.com/en-us/vcpkg/produce/publish-to-a-git-registry#4---update-the-versions-database
