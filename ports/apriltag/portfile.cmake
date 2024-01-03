4511bb34d91a0dc2f44a3a036dd63da0b3fb2f06
Ian Sherman <ian.j.sherman@gmail.com>
2024-01-03 14:58:16 -0800

    Add apriltag

--- ports/apriltag/portfile.cmake
+++ ports/apriltag/portfile.cmake
@@ -1,8 +1,11 @@
+# Support only release builds for now
+set(VCPKG_BUILD_TYPE release)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AprilRobotics/apriltag
-    REF "${VERSION}"
-    SHA512 0  # This is a temporary value. We will modify this value in the next section.
+    REF "v${VERSION}"
+    SHA512 45965f2f888475ea5aecdada0d41a5495f6f0c545d5219c96d82aea009e310a17308df858d4ba40ff191cfe15801dfe16298f20efcc5c117695dd0fa8d3a3fd7
 )
 
 vcpkg_cmake_configure(
@@ -11,8 +14,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "apriltag")
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
